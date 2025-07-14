2ed84a321d54206450fc0925815ae7c0dba3e538
Ragavarthini22 <ragavisk97@gmail.com>
2025-07-14 14:26:52 +0530

    commit
    commit

--- exam_portal/exam/views.py
+++ exam_portal/exam/views.py
@@ -14,6 +14,7 @@ def register_view(request):
     if request.method == 'POST':
         form = RegisterForm(request.POST)
         if form.is_valid():
+            
             form.save()
             messages.success(request, "Registration successful")
             return redirect('login')
