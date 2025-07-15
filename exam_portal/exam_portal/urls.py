cd86782a97f1d176a1c93cd86e0112a703ffbe09
Ragavarthini22 <ragavisk97@gmail.com>
2025-07-15 17:00:53 +0530

    commit
    commit

--- exam_portal/exam_portal/urls.py
+++ exam_portal/exam_portal/urls.py
@@ -16,10 +16,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path,include
+from exam import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('register/',views.register, name='register'),
+    path('register/',views.register_view, name='register'),
     path('verify/', views.verify_otp, name='verify_otp'),
     path('login/', views.login, name='login'),
 
