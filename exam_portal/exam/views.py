e9597b0d035f8fc596179adf9f53232ea75ef563
Ragavarthini22 <ragavisk97@gmail.com>
2025-07-17 20:56:49 +0530

    commit
    commit

--- exam_portal/exam/views.py
+++ exam_portal/exam/views.py
@@ -1,9 +1,10 @@
 from django.shortcuts import render, redirect
-from django.contrib.auth import authenticate, login, logout as auth_logout
+from django.contrib.auth import authenticate, login
 from django.contrib.auth.models import User
 from django.core.mail import send_mail
 from django.conf import settings
 from django.contrib.auth.decorators import login_required
+from django.views.decorators.csrf import csrf_protect 
 import random
 import ssl
 import certifi 
